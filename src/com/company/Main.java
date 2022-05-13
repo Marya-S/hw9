da8ef6f5a7f02aa5968e228b3e5080e752875263
mariastepanova <stepanovams@sibur.ru>
2022-05-13 16:20:51 +0400

    Основные задания 9

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        Author chukovsky = new Author("Корней","Чуковский");
+        Author pushkin = new Author("Александр","Пушкин");
+        Book muha = new Book("Муха-Цокотуха",chukovsky,1923);
+        Book dochka = new Book("Капитанская дочка", pushkin, 1839);
+        dochka.setYear(1841);
     }
 }
